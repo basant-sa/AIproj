0c98fa9330c19572d3e105111354dff6f5918e29
basant <basantebrahem448@gmail.com>
2024-10-15 23:43:33 +0200

    adding recurring reminders

--- myscript.ps1
+++ myscript.ps1
@@ -1,21 +1,31 @@
 $task = Read-Host "What task do you want to be reminded of?"
-$time = Read-Host "Set reminder after how many minutes?"
+$time = [int](Read-Host "Set reminder after how many minutes?")  
+$repeat = [int] (Read-Host "Set reminder interval in minutes (Enter 0 for no recurrence")
 
- 
-$soundAlert = Read-Host "Would you like a sound alert? (y/n)"
+
+$soundAlert = Read-Host "Would you like a sound alert? (yes/no)"
 if ($soundAlert -eq "yes") {
     $sound = $true
 } else {
     $sound = $false
 }
 
- 
-Start-Sleep -Seconds ($time * 60)
-
+function Set-Reminder {
 
 Write-Host "Reminder: $task"
 
-
 if ($sound) {
-    [console]::beep(1000, 500) # Frequency 1000 Hz, duration 500 ms
+    [console]::beep(1000, 500)
+	}
+
 }
+
+Start-Sleep -Seconds ($time * 60)  
+Set-Reminder
+while ($repeat -gt 0)
+{
+	Start-Sleep -Seconds ($time * 60)  
+	Set-Reminder
+
+}
+  
