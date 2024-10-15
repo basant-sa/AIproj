7e849d41a9d6138d8f38bd4de0fa1c7e3dc9ca89
basant <basantebrahem448@gmail.com>
2024-10-15 09:50:40 +0200

    creating my first programme

---
+++ myscript.ps1
@@ -0,0 +1,5 @@
+${Jours De La Semaine} ="lundi","mardi","mercredi","jeudi","vendredi","samedi","dimanche"
+for($i=0;$i-lt ${Jours De La Semaine}.count;$i++){
+Write-Host ${Jours De La Semaine}[$i]
+} 
+Get-Process  | Foreach {$_.Name}  | sort –unique 
